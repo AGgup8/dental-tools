67d8986c2b9be88277c59ffad2069bbe51f72a1b
AGgup8 <110463978+AGgup8@users.noreply.github.com>
2023-01-01 18:24:17 +0000

    fix charting symbol URLs

--- src/components/ChartTooth.vue
+++ src/components/ChartTooth.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="svg-wrapper">
-    <img v-if="symbol" class="symbol" :src="'/src/assets/chart/' + symbol" />
+    <img v-if="isSymbolPresent" class="symbol" :src="symbolUrl" />
 
     <ChartPosteriorSvg
       v-if="type == 1"
@@ -67,78 +67,84 @@ function changeActiveSurface(surface) {
 const calculateType = (number) => (number % 10 >= 4 ? 1 : 2);
 const type = calculateType(props.index);
 
-const symbol = computed(() => {
+const isSymbolPresent = computed(() => {
   if (surfaces.value.whole == 1) {
-    return null;
+    return false;
   } else {
-    if (surfaces.value.whole == 0) {
-      return "missing-line.svg";
-    } else if (surfaces.value.whole == "A") {
-      return "artificial-tooth.svg";
-    } else if (surfaces.value.whole == "BA") {
-      return "bridge-abutment.svg";
-    } else if (surfaces.value.whole == "BP") {
-      return "bridge-pontic.svg";
-    } else if (surfaces.value.whole == "CR") {
-      return "crown.svg";
-    } else if (surfaces.value.whole == "FS") {
-      return "fissure-sealant.svg";
-    } else if (surfaces.value.whole == "F") {
-      return "fracture.svg";
-    } else if (surfaces.value.whole == "FGC") {
-      return "full-gold-crown.svg";
-    } else if (surfaces.value.whole == "FMC") {
-      return "full-metal-crown.svg";
-    } else if (surfaces.value.whole == "IMP") {
-      return "implant.svg";
-    } else if (surfaces.value.whole == "PE") {
-      return "partially-erupted.svg";
-    } else if (surfaces.value.whole == "PBC") {
-      return "porcelain-bonded-crown.svg";
-    } else if (surfaces.value.whole == "PJC") {
-      return "porcelain-jacket-crown.svg";
-    } else if (surfaces.value.whole == "PV") {
-      return "porcelain-veneer.svg";
-    } else if (surfaces.value.whole == "SR") {
-      return "preventative-resin-restoration.svg";
-    } else if (surfaces.value.whole == "RE") {
-      return "recently-extracted.svg";
-    } else if (surfaces.value.whole == "RC") {
-      return "retained-c.svg";
-    } else if (surfaces.value.whole == "SC") {
-      return "space-closed.svg";
-    } else if (
-      surfaces.value.whole == "RM" &&
-      (props.index < 20 || props.index > 40)
-    ) {
-      return "rotated-right-arrow.svg";
-    } else if (
-      surfaces.value.whole == "RM" &&
-      props.index > 20 &&
-      props.index < 40
-    ) {
-      return "rotated-left-arrow.svg";
-    } else if (
-      surfaces.value.whole == "RD" &&
-      (props.index < 20 || props.index > 40)
-    ) {
-      return "rotated-left-arrow.svg";
-    } else if (
-      surfaces.value.whole == "RD" &&
-      props.index > 20 &&
-      props.index < 40
-    ) {
-      return "rotated-right-arrow.svg";
-    } else if (surfaces.value.whole == "IN" && props.index < 30) {
-      return "standing-arrow-down.svg";
-    } else if (surfaces.value.whole == "IN" && props.index > 30) {
-      return "standing-arrow-up.svg";
-    } else if (surfaces.value.whole == "OUT" && props.index < 30) {
-      return "standing-arrow-up.svg";
-    } else if (surfaces.value.whole == "OUT" && props.index > 30) {
-      return "standing-arrow-down.svg";
-    }
+    return true;
   }
+});
+const symbolUrl = computed(() => {
+  let name = "";
+  if (surfaces.value.whole == 0) {
+    name = "missing-line.svg";
+  } else if (surfaces.value.whole == "A") {
+    name = "artificial-tooth.svg";
+  } else if (surfaces.value.whole == "BA") {
+    name = "bridge-abutment.svg";
+  } else if (surfaces.value.whole == "BP") {
+    name = "bridge-pontic.svg";
+  } else if (surfaces.value.whole == "CR") {
+    name = "crown.svg";
+  } else if (surfaces.value.whole == "FS") {
+    name = "fissure-sealant.svg";
+  } else if (surfaces.value.whole == "F") {
+    name = "fracture.svg";
+  } else if (surfaces.value.whole == "FGC") {
+    name = "full-gold-crown.svg";
+  } else if (surfaces.value.whole == "FMC") {
+    name = "full-metal-crown.svg";
+  } else if (surfaces.value.whole == "IMP") {
+    name = "implant.svg";
+  } else if (surfaces.value.whole == "PE") {
+    name = "partially-erupted.svg";
+  } else if (surfaces.value.whole == "PBC") {
+    name = "porcelain-bonded-crown.svg";
+  } else if (surfaces.value.whole == "PJC") {
+    name = "porcelain-jacket-crown.svg";
+  } else if (surfaces.value.whole == "PV") {
+    name = "porcelain-veneer.svg";
+  } else if (surfaces.value.whole == "SR") {
+    name = "preventative-resin-restoration.svg";
+  } else if (surfaces.value.whole == "RE") {
+    name = "recently-extracted.svg";
+  } else if (surfaces.value.whole == "RC") {
+    name = "retained-c.svg";
+  } else if (surfaces.value.whole == "SC") {
+    name = "space-closed.svg";
+  } else if (
+    surfaces.value.whole == "RM" &&
+    (props.index < 20 || props.index > 40)
+  ) {
+    name = "rotated-right-arrow.svg";
+  } else if (
+    surfaces.value.whole == "RM" &&
+    props.index > 20 &&
+    props.index < 40
+  ) {
+    name = "rotated-left-arrow.svg";
+  } else if (
+    surfaces.value.whole == "RD" &&
+    (props.index < 20 || props.index > 40)
+  ) {
+    name = "rotated-left-arrow.svg";
+  } else if (
+    surfaces.value.whole == "RD" &&
+    props.index > 20 &&
+    props.index < 40
+  ) {
+    name = "rotated-right-arrow.svg";
+  } else if (surfaces.value.whole == "IN" && props.index < 30) {
+    name = "standing-arrow-down.svg";
+  } else if (surfaces.value.whole == "IN" && props.index > 30) {
+    name = "standing-arrow-up.svg";
+  } else if (surfaces.value.whole == "OUT" && props.index < 30) {
+    name = "standing-arrow-up.svg";
+  } else if (surfaces.value.whole == "OUT" && props.index > 30) {
+    name = "standing-arrow-down.svg";
+  }
+
+  return new URL(`../assets/chart/${name}`, import.meta.url).href;
 });
 </script>
 
