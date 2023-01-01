c875826800fa6c2b206bed622138a9744d1be29d
AGgup8 <110463978+AGgup8@users.noreply.github.com>
2023-01-01 19:46:24 +0000

    remove duplicate symbol from charting

--- src/modules/chart/components/ChartToothSingle.vue
+++ src/modules/chart/components/ChartToothSingle.vue
@@ -12,12 +12,6 @@
     />
 
     <div class="tooth-wrapper">
-      <img
-        v-if="symbol"
-        class="symbol"
-        :src="'/src/modules/chart/assets/' + symbol"
-      />
-
       <ChartTooth :interactive="true" :index="index" />
 
       <SymbolDropdownList
@@ -93,80 +87,6 @@ function calculateType(number) {
   }
 }
 
-const symbol = computed(() => {
-  if (surfaces.value.whole == 1) {
-    return null;
-  } else {
-    if (surfaces.value.whole == 0) {
-      return "missing-line.svg";
-    } else if (surfaces.value.whole == "A") {
-      return "artificial-tooth.svg";
-    } else if (surfaces.value.whole == "BA") {
-      return "bridge-abutment.svg";
-    } else if (surfaces.value.whole == "BP") {
-      return "bridge-pontic.svg";
-    } else if (surfaces.value.whole == "CR") {
-      return "crown.svg";
-    } else if (surfaces.value.whole == "FS") {
-      return "fissure-sealant.svg";
-    } else if (surfaces.value.whole == "F") {
-      return "fracture.svg";
-    } else if (surfaces.value.whole == "FGC") {
-      return "full-gold-crown.svg";
-    } else if (surfaces.value.whole == "FMC") {
-      return "full-metal-crown.svg";
-    } else if (surfaces.value.whole == "IMP") {
-      return "implant.svg";
-    } else if (surfaces.value.whole == "PE") {
-      return "partially-erupted.svg";
-    } else if (surfaces.value.whole == "PBC") {
-      return "porcelain-bonded-crown.svg";
-    } else if (surfaces.value.whole == "PJC") {
-      return "porcelain-jacket-crown.svg";
-    } else if (surfaces.value.whole == "PV") {
-      return "porcelain-veneer.svg";
-    } else if (surfaces.value.whole == "SR") {
-      return "preventative-resin-restoration.svg";
-    } else if (surfaces.value.whole == "RE") {
-      return "recently-extracted.svg";
-    } else if (surfaces.value.whole == "RC") {
-      return "retained-c.svg";
-    } else if (surfaces.value.whole == "SC") {
-      return "space-closed.svg";
-    } else if (
-      surfaces.value.whole == "RM" &&
-      (props.index < 20 || props.index > 40)
-    ) {
-      return "rotated-right-arrow.svg";
-    } else if (
-      surfaces.value.whole == "RM" &&
-      props.index > 20 &&
-      props.index < 40
-    ) {
-      return "rotated-left-arrow.svg";
-    } else if (
-      surfaces.value.whole == "RD" &&
-      (props.index < 20 || props.index > 40)
-    ) {
-      return "rotated-left-arrow.svg";
-    } else if (
-      surfaces.value.whole == "RD" &&
-      props.index > 20 &&
-      props.index < 40
-    ) {
-      return "rotated-right-arrow.svg";
-    } else if (surfaces.value.whole == "IN" && props.index < 30) {
-      return "standing-arrow-down.svg";
-    } else if (surfaces.value.whole == "IN" && props.index > 30) {
-      return "standing-arrow-up.svg";
-    } else if (surfaces.value.whole == "OUT" && props.index < 30) {
-      return "standing-arrow-up.svg";
-    } else if (surfaces.value.whole == "OUT" && props.index > 30) {
-      return "standing-arrow-down.svg";
-    }
-  }
-});
-
 function findActiveSurface() {
   if (store.getActiveTooth == props.index) {
     return Number(store.getActiveSurface);
@@ -230,14 +150,6 @@ function updateText(text) {
 </script>
 
 <style lang="scss" scoped>
-.symbol {
-  width: 100%;
-  position: absolute;
-  left: 0;
-  top: 0;
-  z-index: 1;
-}
-
 .tooth {
   --tooth-width: 2rem;
   @media only screen and (min-width: 768px) {
@@ -272,18 +184,6 @@ function updateText(text) {
   display: flex;
   position: relative;
   align-items: flex-end;
-
-  &:hover {
-    cursor: pointer;
-  }
-
-  &:hover .symbol {
-    z-index: 0;
-  }
-
-  &:hover .tooth-svg {
-    z-index: 1;
-  }
 }
 
 .tooth-svg {
